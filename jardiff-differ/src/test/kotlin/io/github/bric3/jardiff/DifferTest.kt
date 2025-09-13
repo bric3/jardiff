34acdac83ead01dc380fb8491c8feff56759608c
Brice Dutheil <brice.dutheil@gmail.com>
2025-09-14 01:55:30 +0200

    test: New differ tests

--- jardiff-differ/src/test/kotlin/io/github/bric3/jardiff/DifferTest.kt
+++ jardiff-differ/src/test/kotlin/io/github/bric3/jardiff/DifferTest.kt
@@ -11,9 +11,14 @@
 package io.github.bric3.jardiff
 
 import io.github.bric3.jardiff.Logger.Companion.green
+import io.github.bric3.jardiff.Logger.Companion.red
+import io.github.bric3.jardiff.OutputMode.diff
+import io.github.bric3.jardiff.OutputMode.simple
+import net.bytebuddy.build.ToStringPlugin.Exclude
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.io.TempDir
+import java.awt.Color.red
 import java.io.StringWriter
 import java.nio.file.Path
 
@@ -22,27 +27,91 @@ class DifferTest {
     lateinit var tempDir: Path
 
     @Test
-    fun `should detect no differences interface same jar`() {
+    fun `should detect no differences in same jar with simple mode`() {
         val singleClassJar = createJarFromResources(
             tempDir,
             FooFixtureClass::class.java.classLoader,
             FooFixtureClass::class.path
         )
-        val output = StringWriter()
 
-        Differ(
-            Logger(output, StringWriter(), verbosity(0)),
-            OutputMode.simple,
-            PathToDiff.of(PathToDiff.LeftOrRight.LEFT, singleClassJar),
-            PathToDiff.of(PathToDiff.LeftOrRight.RIGHT, singleClassJar),
-        ).diff()
+        val output = diff(simple, singleClassJar, singleClassJar)
 
-        assertThat(output.toString().trim()).isEqualTo(
+        assertThat(output).isEqualTo(
             """
             ${green("✔")}️ META-INF/MANIFEST.MF
             ${green("✔")}️ io/github/bric3/jardiff/FooFixtureClass.class
             """.trimIndent()
         )
+    }
+
+    @Test
+    fun `should detect no differences in same jar with diff mode`() {
+        val singleClassJar = createJarFromResources(
+            tempDir,
+            FooFixtureClass::class.java.classLoader,
+            FooFixtureClass::class.path
+        )
+
+        val output = diff(diff, singleClassJar, singleClassJar)
+
+        assertThat(output).isEmpty()
+    }
+
+    @Test
+    fun `should detect single differences between jar and directory with simple mode`() {
+        val singleClassJar = createJarFromResources(
+            tempDir,
+            FooFixtureClass::class.java.classLoader,
+            FooFixtureClass::class.path
+        )
+
+        val output = diff(simple, FooFixtureClass::class.location, singleClassJar, excludes = setOf("*.md", "*.properties"))
+
+        assertThat(output).isEqualTo(
+            """
+            ${red("⨯")} META-INF/MANIFEST.MF
+            ${red("⨯")} META-INF/jardiff-differ_testFixtures.kotlin_module
+            ${green("✔")}️ io/github/bric3/jardiff/FooFixtureClass.class
+            """.trimIndent()
+        )
+    }
+
+    @Test
+    fun `should detect single differences between jar and directory with diff mode`() {
+        val singleClassJar = createJarFromResources(
+            tempDir,
+            FooFixtureClass::class.java.classLoader,
+            FooFixtureClass::class.path
+        )
+
+        val output = diff(diff, FooFixtureClass::class.location, singleClassJar, excludes = setOf("*.md", "*.properties"))
+
+        assertThat(output).isEqualTo(
+            """
+            --- META-INF/MANIFEST.MF
+            +++ META-INF/MANIFEST.MF
+            @@ -1,2 +1,1 @@
+            -Manifest-Version: 1.0
+            -
+            +BINARY FILE SHA-1: ba8ab5a0280b953aa97435ff8946cbcbb2755a27
+            --- META-INF/jardiff-differ_testFixtures.kotlin_module
+            +++ /dev/null
+            @@ -1,1 +1,0 @@
+            -BINARY FILE SHA-1: c4ab3f5c96ccba90c685717137ef543a3b2c30d9
+            """.trimIndent()
+        )
+    }
+
+    private fun diff(outputMode: OutputMode, left: Path, right: Path, excludes: Set<String> = emptySet()): String {
+        val output = StringWriter()
+        Differ(
+            logger = Logger(output, StringWriter(), verbosity(0)),
+            outputMode = outputMode,
+            left = PathToDiff.of(PathToDiff.LeftOrRight.LEFT, left),
+            right = PathToDiff.of(PathToDiff.LeftOrRight.RIGHT, right),
+            excludes = excludes
+        ).diff()
+        return output.toString().trim()
     }
 
     private fun verbosity(level: Int) = BooleanArray(level)
